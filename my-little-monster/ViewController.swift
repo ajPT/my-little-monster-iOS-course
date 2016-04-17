317164cafbea6dbb3d6d16b59cf4b884dd2a707c
Amadeu Andrade <amadeuandrade@Amadeus-MBP.lan>
2016-04-17 23:32:09 +0100

    Added monster animation

--- my-little-monster/ViewController.swift
+++ my-little-monster/ViewController.swift
@@ -10,14 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monsterImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        var imgArray = [UIImage]()
+        
+        for index in 1...4 {
+            let img = UIImage(named: "idle (\(index))")
+            imgArray.append(img!)
+        }
+        
+        monsterImg.animationImages = imgArray
+        monsterImg.animationDuration = 0.8
+        monsterImg.animationRepeatCount = 0 //0 = infinite
+        monsterImg.startAnimating()
+        
+        
     }
 
 
